775636e619109f6e2f007034b42106878c36dfd3
XeliteXirish <shaunoneill7@me.com>
2015-06-22 12:35:49 +0100

    Basic feature system

---
+++ src/main/java/cf/mcdTeam/Immersion/base/ResourceProvider.java
@@ -0,0 +1,9 @@
+package cf.mcdTeam.Immersion.base;
+
+/**
+ * Created by XeliteXirish on 22/06/2015.
+ *
+ * @Author XeliteXirish
+ */
+public class ResourceProvider {
+}
---
+++ src/main/java/cf/mcdTeam/Immersion/features/Feature.java
@@ -0,0 +1,9 @@
+package cf.mcdTeam.Immersion.features;
+
+/**
+ * Created by XeliteXirish on 22/06/2015.
+ *
+ * @Author XeliteXirish
+ */
+public interface Feature {
+}
